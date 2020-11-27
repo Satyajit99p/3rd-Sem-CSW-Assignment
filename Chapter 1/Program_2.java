42aeff2d51fb0cacafe512055983eb2fc108579b
Satyajit Panigrahi <satyapani999@gmail.com>
2020-11-27 09:08:01 +0530

    Add files via upload

---
+++ Chapter 1/Program_2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Program_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int s=x+y;
+		System.out.println(s);
+		
+
+	}
+
+}
