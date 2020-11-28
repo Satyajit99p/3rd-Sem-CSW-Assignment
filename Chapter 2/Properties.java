a5c6eef84377008041ad3ea5fdf7c80e606da534
Satyajit Panigrahi <satyapani999@gmail.com>
2020-11-28 17:46:08 +0530

    Add files via upload

---
+++ Chapter 2/Properties.java
@@ -0,0 +1,13 @@
+
+public class Properties {
+
+	public static void main(String[] argv) {
+		// TODO Auto-generated method stub
+		System.out.println("OS name="+System.getProperty("os.name"));
+		System.out.println("VERSION="+System.getProperty("os.version"));
+		System.out.println("architecture="+System.getProperty("os.arch"));
+		System.out.println(("CLASS PATH="+System.getProperty("java.class.path")));
+
+	}
+
+}
