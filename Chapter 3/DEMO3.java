1e88eb951d971106a1b11df56acf83da53d72b5f
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-05 12:38:57 +0530

    Add files via upload

---
+++ Chapter 3/DEMO3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class DEMO3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner (System.in);
+		String s1=sc.nextLine();
+		String s2="Hello World";
+		System.err.println("equals() methods output : "+s1.equals(s2));
+		System.err.println("equalsIgnoreCase() method output : "+s1.equalsIgnoreCase(s2));
+
+	}
+
+}
