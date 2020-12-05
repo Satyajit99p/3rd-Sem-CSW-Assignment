b9cf609630cb15bf9d8c88d6efea9383da6cdbfa
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-05 12:38:03 +0530

    Add files via upload

---
+++ Chapter 3/Contain.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Contain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		String s2=sc.nextLine();
+		System.out.println(s1.contains(s2));
+	}
+
+}
