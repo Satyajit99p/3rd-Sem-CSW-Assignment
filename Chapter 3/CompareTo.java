00c29766fd00dec8928b2b138fc86a631e55b157
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-05 12:37:16 +0530

    Add files via upload

---
+++ Chapter 3/CompareTo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CompareTo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner (System.in);
+		String s1=sc.nextLine();
+		String s2=sc.nextLine();
+		System.out.println(s1.compareTo(s2));
+
+	}
+
+}
