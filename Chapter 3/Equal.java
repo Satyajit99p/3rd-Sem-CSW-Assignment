7085b60cb0e81bc09acd1f44ef39c81483331984
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-06 18:24:33 +0530

    Add files via upload

---
+++ Chapter 3/Equal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Equal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		String s2=sc.nextLine();
+		System.out.println(s1.equalsIgnoreCase(s2));
+		
+
+	}
+
+}
