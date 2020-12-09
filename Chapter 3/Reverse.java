284a4ba968034db7dcd9a4260d08d960eea562a8
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-09 19:16:16 +0530

    Add files via upload

---
+++ Chapter 3/Reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		StringBuilder st=new StringBuilder(s);
+		System.out.println(st.reverse());
+		
+		
+	}
+	}
+
