8b6c037de244527788bddcca02d3f922a3fbb3e1
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-11 20:51:39 +0530

    Add files via upload

---
+++ Chapter 3/Trail_space.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Trail_space {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner (System.in);
+		String s=sc.nextLine();
+		s=s.trim();
+		System.out.println(s);
+
+	}
+
+}
