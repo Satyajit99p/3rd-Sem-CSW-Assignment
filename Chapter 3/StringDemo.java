b91321c67a665850852f92515457f014da12900f
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-11 20:49:58 +0530

    Add files via upload

---
+++ Chapter 3/StringDemo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		System.out.println(s.substring(4));
+		System.out.println(s.substring(0,s.length()));
+		
+	}
+
+}
