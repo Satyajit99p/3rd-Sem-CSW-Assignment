fc3a85010d8b47e8cbab6c3e3542b7871fc6a49d
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-11 20:52:00 +0530

    Add files via upload

---
+++ Chapter 3/Vowels.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Vowels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner (System.in);
+		String s=sc.nextLine();
+		int c=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)=='A' 
+					|| s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='I')
+				c++;
+		}
+		System.out.println(c);
+
+	}
+
+}
