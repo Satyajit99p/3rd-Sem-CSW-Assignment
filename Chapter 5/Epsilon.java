0d1e1e4eba7c3978df8e637191ece7853cc12c09
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-18 18:43:36 +0530

    Add files via upload

---
+++ Chapter 5/Epsilon.java
@@ -0,0 +1,11 @@
+
+public class Epsilon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 final double EPSILON = 0.001;
+		    for(double d=0.0;d<=1;d=d+0.1+EPSILON) {
+		      System.out.println(d);
+	}
+	}
+}
