93c0a3893ec171c3e3e437fe73a9afc0f15de961
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-19 18:23:15 +0530

    Add files via upload

---
+++ Chapter 5/Integer_string.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Integer_string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		String s=String.valueOf(x);
+		System.out.println(s);
+		
+
+	}
+
+}
