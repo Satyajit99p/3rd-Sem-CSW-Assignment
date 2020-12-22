d0449a915779836e47c71300abba82299bc68455
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-22 07:56:55 +0530

    Add files via upload

---
+++ Chapter 5/Negative.java
@@ -0,0 +1,16 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.*;
+public class Negative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		double x=sc.nextDouble();
+		NumberFormat form=new DecimalFormat("-");
+		
+		System.out.println(form.format(x));
+
+	}
+
+}
