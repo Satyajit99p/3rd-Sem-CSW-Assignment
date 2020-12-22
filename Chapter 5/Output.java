91aa9798ead4319c8c13e99447120bcb52060ec7
Satyajit Panigrahi <satyapani999@gmail.com>
2020-12-22 07:57:46 +0530

    Add files via upload

---
+++ Chapter 5/Output.java
@@ -0,0 +1,11 @@
+
+public class Output {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double d;
+		for (d=0.0;d<=1.0;d+=0.1)
+			System.out.println(d);
+	}
+
+}
